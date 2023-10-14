d57875d3ffb9a41e1f6af653255888dbb723ec82
matahho <mata1381@gmail.com>
2023-10-14 21:06:48 +0330

    print group members and the group number in boot page .

--- init.c
+++ init.c
@@ -21,6 +21,11 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1, "GROUP #20 :\n");
+    printf(1, "1. SMahdi HajiSeyedHossein\n");
+    printf(1, "2. Alireza Hosseini\n");
+    printf(1, "3. Amirali Shahriary\n\n");
+    
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
