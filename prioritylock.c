f3447573c0b85467a485a09ff71b72c0b95b3316
mahdi <mahdi@OS.Ubuntu.myguest.virtualbox.org>
2023-12-31 10:29:58 +0330

    add to queue

--- prioritylock.c
+++ prioritylock.c
@@ -10,17 +10,20 @@
 #include "prioritylock.h"
 
 void
-initplock(struct prioritylock *lk, char *name)
+initsleeplock(struct prioritylock *lk, char *name)
 {
-
+  initlock(&lk->lk, "sleep lock");
   lk->name = name;
   lk->locked = 0;
   lk->pid = 0;
-  lk->priority = 0;
+  lk->priorityQueue = NULL;
+
 }
 
-void acquirep(struct prioritylock *lk)
-{
+
+void 
+add_to_pq (struct proc* head , struct proc* proc){
 	
-
 }
+
+
